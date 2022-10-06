f3cbb0b6163cbe392b2914e7a68b08e28a10cce1
Kirill P <pak.kiruha@yandex.ru>
2022-10-07 01:23:27 +0300

    delete old not used code

--- src/main/java/ru/netology/servlet/MainServlet.java
+++ src/main/java/ru/netology/servlet/MainServlet.java
@@ -16,9 +16,6 @@ public class MainServlet extends HttpServlet {
 
     @Override
     public void init() {
-        //final var repository = new PostRepository();
-        //final var service = new PostService(repository);
-        //controller = new PostController(service);
         final var context = new AnnotationConfigApplicationContext("ru.netology");
         controller =  context.getBean(PostController.class);
     }
